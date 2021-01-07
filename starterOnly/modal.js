e623e4617dc8da5d9ddb3c9269ab46afff911753
Andreea Chelaru <andreeache@gmail.com>
2021-01-07 14:07:54 +0100

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,33 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+// match the close class button
+const modalClose = document.querySelectorAll(".close")
 const formData = document.querySelectorAll(".formData");
+
+// form elements
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const tournaments = document.getElementById("quantity");
+const locations = document.querySelectorAll(".checkbox-input")
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// add the close form action
+modalClose.forEach((btn) => btn.addEventListener("click", hideModal))
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// hide the modal form
+function hideModal() {
+  modalbg.style.display = "none";
 }
 
 
