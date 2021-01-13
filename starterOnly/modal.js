3165134f772db06c0d737defbbd6131a2154eda1
Andreea Chelaru <andreeache@gmail.com>
2021-01-13 10:19:56 +0100

    fixed the closed button in the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,18 +20,53 @@ const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
 const tournaments = document.getElementById("quantity");
-const locations = document.querySelectorAll(".checkbox-input")
-
+const locations = document.querySelectorAll(".checkbox-input");
+const terms = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // add the close form action
-modalClose.forEach((btn) => btn.addEventListener("click", hideModal))
+modalClose.forEach((btn) => btn.addEventListener("click", hideModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// email validation - copy pasted from w3resource.com
+function ValidateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
+}
+
+// submit function
+function validateForm(){
+  if (firstName.value == null || firstName.value == "" || lastName.value == null || lastName.value == "") {
+    return false;
+  }
+
+  if (email.value == null || email.value == "" || !ValidateEmail(email.value)) {
+    return false;
+  }
+
+  if (birthdate.value == null || birthdate.value == "") {
+    return false;
+  }
+
+  if (tournaments.value == null || tournaments.value == "") {
+    return false;
+  }
+
+  if (!terms.checked) {
+    return false;
+  }
+
+  return true;
 }
 
 // hide the modal form
